612caa7a80ef5e3fedd4b064e575d926bf5234a8
ElBerserker <70364040+ElBerserker@users.noreply.github.com>
2021-05-21 11:42:07 -0500

    Add files via upload

---
+++ IntegralDefinida.m
@@ -0,0 +1,28 @@
+%Autor: Hernandez Lopez Raul @Neo
+%Correo: freeenergy1975@gmail.com
+%Tema: Integrales definidas.
+
+
+%limiar pantalla y variables 
+clc 
+clear 
+
+%carga el paquete symbolics
+pkg load symbolic
+
+%crea un variable simbolica 
+syms x
+
+%obtencion de datos
+f = input('Funcion :')
+limInf = input ('Limite Inferior :');
+limSup = input ('Limite Superior :');
+
+puntos = limInf:0.1:limSup;
+
+%impresion de la funcion primitiva
+funcion_primitiva = int(f)
+%impresion del valor de la integral entre a y b
+int(f, limInf, limSup)
+%imprime la funcion primitiva
+ezplot(funcion_primitiva);
