29258d0160313ca1a6d7a8f7ad42af37e06b85e7
ElBerserker <70364040+ElBerserker@users.noreply.github.com>
2021-06-23 13:47:23 -0500

    Add files via upload

---
+++ PropiedadesDeLaIntegralDefinida1.m
@@ -0,0 +1,9 @@
+% Nombre: Hernandez Lopez Raul
+% Correo: freeenergy1975@gmail.com
+syms x;
+f = x;
+a = -2;
+b = 3;
+
+int(f, a, b)
+ezplot(f)
---
+++ PropiedadesDeLaIntegralDefinida2.m
@@ -0,0 +1,10 @@
+% Nombre: Hernandez Lopez Raul
+% Correo: freeenergy1975@gmail.com
+syms x;
+f = (2*(x.^2))+3;
+a = 2;
+b = 0;
+
+int(f, a, b)
+ezplot(f)
+
